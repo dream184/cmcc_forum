9e19192601e5ae9adcdfe8204e23a59355784e0e
shibao <dream0184@yahoo.com.tw>
2022-01-05 22:51:19 +0800

    debug: fixed timezone diplay issue

--- config/handlebars-helpers.js
+++ config/handlebars-helpers.js
@@ -1,10 +1,12 @@
 const dayjs = require('dayjs')
+var timezone = require('dayjs/plugin/timezone')
+dayjs.extend(timezone)
 require('dayjs/locale/zh-tw')
-dayjs.locale('zh-tw') 
+dayjs.locale('zh-tw')
 
 module.exports = {
   dayjs: function (time) {
-    return dayjs(time).format('YYYY/MM/DD HH:mm')
+    return dayjs(time).tz("Asia/Taipei").format('YYYY/MM/DD HH:mm')
   },
   rankingStar: function (number) {
     const star = '<i class="fas fa-star text-warning"></i>'
