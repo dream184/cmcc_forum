cb89c0f4bbe086f08e2adbd97dfa266087a5957a
shibao <dream0184@yahoo.com.tw>
2022-02-04 18:38:06 +0800

    refactor: refactor feedbackController

--- controllers/feedbackController.js
+++ controllers/feedbackController.js
@@ -22,17 +22,29 @@ const feedbackController = {
       req.flash('error_messages', '回饋欄不能空白')
       return res.redirect('back')
     }
-    return Feedback.create({
-      feedback,
-      isMentor: false,
-      VoicefileId: req.params.id,
-      UserId: req.user.id
-    })
+
+    return Voicefile.findByPk(req.params.id)
+      .then((voicefile) => {
+        if (!voicefile) {
+          req.flash('error_messages', '該音檔不存在，無法回饋')
+          return res.redirect('back')
+        }
+        return Feedback.create({
+          feedback,
+          isMentor: false,
+          VoicefileId: req.params.id,
+          UserId: req.user.id
+        })
+      })
       .then(() => {
         req.flash('success_messages', '已經送出回饋')
         return res.redirect('back')
-      })    
-     
+      })
+      .catch(err => {
+        req.flash('error_messages', '回饋失敗')
+        console.log(err)
+        return res.redirect('back')
+      })
   },
   editFeedback: (req, res) => {
     return Feedback.findByPk(req.params.id, {
@@ -73,6 +85,11 @@ const feedbackController = {
             return res.redirect(`/classes/${feedback.Voicefile.ClassId}/homeworks/${feedback.Voicefile.Homework.id}/voicefiles/${feedback.Voicefile.id}/feedbacks`)
           })
       })
+      .catch(err => {
+        req.flash('error_messages', '回饋修改失敗')
+        console.log(err)
+        return res.redirect('back')
+      })
   },
   deleteFeedback: (req, res) => {
     return Feedback.findByPk(req.params.id)
@@ -82,10 +99,15 @@ const feedbackController = {
           return res.redirect('back')
         }
         return feedback.destroy()
-          .then(() => {    
-            req.flash('success_messages', '已經成功刪除回饋')
-            return res.redirect('back')
-          })
+      })
+      .then(() => {    
+        req.flash('success_messages', '已經成功刪除回饋')
+        return res.redirect('back')
+      })
+      .catch(err => {
+        req.flash('error_messages', '無法刪除回饋')
+        console.log(err)
+        return res.redirect('back')
       })
   },
   getAdminFeedbacks: (req, res) => {
@@ -129,7 +151,11 @@ const feedbackController = {
             req.flash('success_messages', '已經送出點評回饋')
             return res.redirect('back')
           })
-          .catch(err => console.log(err))    
+          .catch(err => {
+            req.flash('error_messages', '回饋失敗')
+            console.log(err)
+            return res.redirect('back')
+          })   
       }) 
   },
   editAdminFeedback: (req, res) => {
@@ -175,6 +201,11 @@ const feedbackController = {
             req.flash('success_messages', '回饋修改成功')
             return res.redirect(`/admin/voicefiles/${feedback.VoicefileId}/feedbacks`)
           })
+      })
+      .catch(err => {
+        req.flash('error_messages', '回饋修改失敗')
+        console.log(err)
+        return res.redirect('back')
       })
   },
 }
